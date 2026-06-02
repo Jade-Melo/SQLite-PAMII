3d664f4f6b44c0cac296d81df013954ef634046f
billafranca <bragap379@gmail.com>
2026-06-02 11:24:12 -0300

    Continuando o projeto de banco de dados nativo do Android.

--- app/src/main/java/com/jade/jadeetimsqlite/controller/ClienteController.java
+++ app/src/main/java/com/jade/jadeetimsqlite/controller/ClienteController.java
@@ -1,11 +1,49 @@
 package com.jade.jadeetimsqlite.controller;
 
+import android.content.ContentValues;
 import android.content.Context;
 
+import com.jade.jadeetimsqlite.datamodel.ClienteDataModel;
 import com.jade.jadeetimsqlite.datasource.AppDataBase;
+import com.jade.jadeetimsqlite.model.Cliente;
 
-public class ClienteController extends AppDataBase {
+public class ClienteController extends AppDataBase implements iCRUD<Cliente> {
+    
+    ContentValues dados;
     public ClienteController(Context context) {
         super(context);
+    }
+    
+    public boolean inserir(Cliente obj){
+        dados = new ContentValues();
+        dados.put(ClienteDataModel.NOME,obj.getNome());
+        dados.put(ClienteDataModel.EMAIL,obj.getEmail());
+        
+        String tabela = ClienteDataModel.TABELA;
+        return inserir(tabela, dados);
+    }
+
+    private boolean inserir(String tabela, ContentValues dados) {
+        return false;
+    }
+
+    @Override
+    public boolean incluir(Cliente obj) {
+        return false;
+    }
+
+    @Override
+    public boolean alterar(Cliente obj) {
+        return false;
+    }
+
+    @Override
+    public boolean deletar(Cliente obj) {
+        return false;
+    }
+
+    @Override
+    public void listar(Cliente obj) {
+
     }
 }
